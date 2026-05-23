5394187fbc9a06a4d67cb78e9272987ba1b5f000
Shruti@2050 <shrutijoshi1208@gmail.com>
2026-05-23 07:16:24 +0530

    Java Core programs

---
+++ Java Core/EvenOdd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class EvenOdd {
+    public static void main(String[] args) {    
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt(); 
+        if (number % 2 == 0) {    
+            System.out.println(number + " is an even number.");    
+        } else {    
+            System.out.println(number + " is an odd number.");    
+        }    
+        sc.close();
+    }
+}
