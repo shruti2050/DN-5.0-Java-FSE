fba49e2346af2a5f042721cbefb952b316bb211a
Shruti@2050 <shrutijoshi1208@gmail.com>
2026-05-23 09:51:52 +0530

    leap year using nested if else

---
+++ Java Core/LeapYear.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class LeapYear{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+        int year = sc.nextInt();
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    System.out.println("year is leap year");
+                }else{
+                    System.out.println("Not leap year");
+                }
+            }else{
+                System.out.println("leap year");
+            }
+        }else{
+            System.out.println("Not leap year");
+        }
+    }
+}
